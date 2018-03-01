ae00f065e777fcb172566bb48badfc17ccef911b
Jimmy Chang <jimmyfchang94@gmail.com>
2018-03-01 11:55:06 -0800

    testing

--- js/app.js
+++ js/app.js
@@ -43,6 +43,56 @@ function render() {
   }
 }
 
+// Selfie
+
+var selfiePic = '';
+(function() {
+  var selfieCam, takeSelfie, imageProcessor, selfies, takeSelfieButton;
+  function startup(){
+    selfieCam = document.getElementById('selfie-cam');
+    takeSelfieButton = document.getElementById('takeSelfieButton'),
+    imageProcessor = document.getElementById('image-processor');
+    selfies = document.getElementById('selfies');
+
+    imageProcessor.width = 640;
+    imageProcessor.height = 480;
+
+    navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({
+        video:true,
+        audio:false
+      }, function (stream) {
+        window.URL = window.URL || webkitURL;
+        var streamURL = window.URL.createObjectURL(stream);
+        selfieCam.src = streamURL;
+        selfieCam.play();
+      }, function (error) {
+        console.log(error);
+      });
+    }
+
+    takeSelfieButton.addEventListener('click', takeSelfie);
+  }
+  function takeSelfie() {
+    var context = imageProcessor.getContext('2d');
+    console.log(selfieCam);
+    context.drawImage(selfieCam, 0, 0, 640, 480);
+    var imageURL = imageProcessor.toDataURL();
+
+    var img = document.createElement('img');
+    img.setAttribute('src', imageURL);
+		console.log();
+		selfiePic = imageURL;
+    selfies.appendChild(img);
+  }
+  window.addEventListener('load', startup);
+})();
+
+
+
+
 //eventhandler
 function playerEvent(event) {
 
@@ -79,6 +129,7 @@ function playerEvent(event) {
   for (var i = 0; i < nameArray.length; i ++) {
     new Player(nameArray[i], playerIdArray[i]);
     playerArray[i].filepath = playerIcon[i];
+
   }
 
   var playerArrayStrigify = JSON.stringify(playerArray);
